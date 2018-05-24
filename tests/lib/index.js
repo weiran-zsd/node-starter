2e018092a65bbe2f74845ecb9d428159f0496b4f
薛定谔的猫 <hh_2013@foxmail.com>
2018-05-25 07:15:15 +0800

    chore: test using dist/lib.js

--- tests/lib/index.js
+++ tests/lib/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const assert = require('assert');
-const add = require('../../lib/index.js');
+const add = require('../../dist/lib.js');
 
 describe('add', () => {
   it('1 + 1 === 2', () => {
