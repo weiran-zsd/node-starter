7c2d4897615ac2dc42ad89aa37bd55b406ed2062
唯刃 <weiren@dian.so>
2021-03-10 11:01:29 +0800

    feat: add prettier & standard-version

--- tests/lib/index.js
+++ tests/lib/index.js
@@ -1,11 +1,10 @@
-'use strict';
+"use strict";
 
-const assert = require('assert');
-const add = require('../../lib/index.js');
+const assert = require("assert");
+const add = require("../../lib/index.js");
 
-describe('add', () => {
-  it('1 + 1 === 2', () => {
+describe("add", () => {
+  it("1 + 1 === 2", () => {
     assert.strictEqual(add(1, 1), 2);
   });
 });
-
